ca9f5a11ffa7065ac33c17681c0613b9f73f2556
merch <58620696+a-discount@users.noreply.github.com>
2020-08-01 17:35:56 -0700

    Delete code.js

--- code.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("myCode").addEventListener("click", myFunction);
-    function myFunction() {
-  window.open("https://github.com/a-discount/Chromebook-Browser-KRUNKER-CLIENT", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=675,height=400");
-}
