8f5c5dc2812e14d375e8cd238e9c8cba8c64dc4c
Forgiving <forgiving@forgiving.ml>
2020-09-23 11:27:22 -0700

    github name update

--- js/code.js
+++ js/code.js
@@ -1,4 +1,4 @@
 document.getElementById("myCode").addEventListener("click", myFunction);
     function myFunction() {
-  window.open("https://github.com/a-discount/Chromebook-Browser-KRUNKER-CLIENT", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=675,height=400");
+  window.open("https://github.com/forgivings/Chromebook-Browser-KRUNKER-CLIENT", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=675,height=400");
 }
